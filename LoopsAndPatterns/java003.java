6d1253b4808cbcd852174dbf81a4c8586d3b4463
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-01-21 18:42:58 +0530

    Hollow and inverted roted half pyramid

---
+++ LoopsAndPatterns/java003.java
@@ -0,0 +1,38 @@
+package LoopsAndPatterns;
+
+import java.util.Scanner;
+
+public class java003 {
+    public static void hollowRectangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 ||  i==n || j==n || j==1 ){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void invertedAndRotatedHalfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter number to print pattern : ");
+        int n=sc.nextInt();
+        //hollowRectangle(n);
+        //invertedAndRotatedHalfPyramid(n);
+        invertedAndHalfPyramid(n);
+    }
+}
