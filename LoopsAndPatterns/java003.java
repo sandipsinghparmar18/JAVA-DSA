898f34c1986272472a751d827aaf088031d38bb1
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-01-21 19:18:08 +0530

    hollowRhombus pattern

--- LoopsAndPatterns/java003.java
+++ LoopsAndPatterns/java003.java
@@ -91,6 +91,21 @@ public class java003 {
             System.out.println();
         }
     }
+    public static void hollowRhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter number to print pattern : ");
@@ -101,6 +116,7 @@ public class java003 {
         //floydsTraingle(n);
         //zeroOneTraingle(n);
         //butterfly(n);
-        solidRhombus(n);
+        //solidRhombus(n);
+        hollowRhombus(n);
     }
 }
