5ee5c96bf73c32da9aba9c1c6324059ad09c87c0
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-01-21 18:46:14 +0530

    invertedAndHalfPyramid

--- LoopsAndPatterns/java003.java
+++ LoopsAndPatterns/java003.java
@@ -26,7 +26,14 @@ public class java003 {
             System.out.println();
         }
     }
-    
+    public static void invertedAndHalfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter number to print pattern : ");
